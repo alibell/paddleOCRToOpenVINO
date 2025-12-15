623dfec190d2588bc3d77552d5ac7b4b625217f9
Ali Bellamine <bellamine@elsan.care>
2025-12-15 10:51:23 +0100

    Also copy config file

--- convert_model.sh
+++ convert_model.sh
@@ -4,22 +4,26 @@ set -e
 model_name=$1
 filename=$(basename "$model_name")
 model_path=./original_models/$model_name
-model_onnx_path=./onnx_models/$filename/$filename.onnx
+model_onnx_path=./onnx_models/$filename/
 model_openvino_path=./openvino_models/$filename/
 
-mkdir -p onnx_models/$filename/
-mkdir -p openvino_models/$filename/
+mkdir -p $model_onnx_path && \
+mkdir -p $model_openvino_path
 
 # Download model
 echo "uvx hf download $model_name --local-dir $model_path"
 uvx hf download $model_name --local-dir $model_path
+
+# Copy config file
+cp $model_path/config.json $model_onnx_path/config.json
+cp $model_path/config.json $model_openvino_path/config.json
 
 # Convert to ONNX
 .venv/bin/paddle2onnx \
 --model_dir $model_path \
 --model_filename inference.json \
 --params_filename inference.pdiparams \
---save_file $model_onnx_path
+--save_file $model_onnx_path/model.onnx
 
 # Convert to OpenVino
 uvx --from openvino ovc $model_onnx_path --output_model $model_openvino_path
