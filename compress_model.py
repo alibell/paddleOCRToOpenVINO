f8736e40b4c7d309acd8cbeeaa1366e186ef4724
Ali Bellamine <bellamine@elsan.care>
2025-12-22 10:01:38 +0100

    Add INT8 compression

---
+++ compress_model.py
@@ -0,0 +1,71 @@
+import openvino as ov
+from nncf import compress_weights, CompressWeightsMode
+import argparse
+import os
+from typing import Literal
+from pathlib import Path
+
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    "--input_dir",
+    type=Path,
+)
+parser.add_argument(
+    "--output_dir",
+    type=Path,
+)
+
+# Compression mode
+# More info:
+#   https://github.com/openvinotoolkit/nncf/blob/develop/docs/usage/post_training_compression/weights_compression/Usage.md#mixed-precision-modes
+parser.add_argument(
+    "--compression_mode",
+    type=Literal[
+        'FP4',
+        'FP8_E4M3',
+        'INT4_ASYM',
+        'INT4_SYM',
+        'INT8',
+        'INT8_ASYM',
+        'INT8_SYM',
+        'MXFP4',
+        'MXFP8_E4M3',
+        'NF4',
+    ],
+    default="INT8_SYM"
+)
+
+# Load CLI args
+args = parser.parse_args()
+compression_mode = getattr(
+    CompressWeightsMode,
+    args.compression_mode
+)
+
+# Load OpenVino inference engine
+core = ov.Core()
+
+# Load model
+model = core.read_model(
+    model=str(
+        args.input_dir /
+        "model.xml"
+    ),
+    weights=str(
+        args.input_dir /
+        "model.bin"
+    ),
+)
+
+# Compress model
+compressed_model = compress_weights(
+    model=model,
+    model=compression_mode,
+)
+
+# Save model
+os.makedirs(args.output_dir, exist_ok=True)
+ov.save_model(
+    compressed_model,
+    str(args.output_dir)
+)
--- convert_model.sh
+++ convert_model.sh
@@ -5,7 +5,7 @@ model_name=$1
 filename=$(basename "$model_name")
 model_path=./original_models/$model_name
 model_onnx_path=./onnx_models/$filename/
-model_openvino_path=./openvino_models/$filename/
+model_openvino_path=./openvino_models/$filename
 
 mkdir -p $model_onnx_path && \
 mkdir -p $model_openvino_path
@@ -30,8 +30,14 @@ cp $model_path/config.json $model_openvino_path/config.json
 uvx -p 3.11 onnxoptimizer ./$filename.model.raw.onnx $model_onnx_path/model.onnx
 
 # Convert to OpenVino
-uvx --from openvino ovc $model_onnx_path/model.onnx --compress_to_fp16=True --output_model $model_openvino_path || true
+uvx --from openvino ovc $model_onnx_path/model.onnx --compress_to_fp16=True --output_model $model_openvino_path/ || true
+
+# Create OpenVino 8 bit model
+mkdir -p $model_openvino_path.int8
+cp $model_path/config.json $model_onnx_path.int8/config.json
+uv run compress_model --input_dir $model_onnx_path --output_dir $model_onnx_path.int8 --compression_mode INT8_SYM
 
 # Build artifact
 tar -czf ./artifacts/$filename.onnx.tar.gz -C $(dirname "$model_onnx_path") $(basename "$model_onnx_path")
 tar -czf ./artifacts/$filename.openvino.tar.gz -C $(dirname "$model_openvino_path") $(basename "$model_openvino_path")
+tar -czf ./artifacts/$filename.int8.openvino.tar.gz -C $(dirname "$model_openvino_path.int8") $(basename "$model_openvino_path.int8")
